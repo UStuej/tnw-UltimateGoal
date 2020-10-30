ce5b78783aa179860b15c48c306d98e9945dbceb
UStuej <45504535+UStuej@users.noreply.github.com>
2020-10-29 21:19:48 -0500

    [HOTFIX] Refactor DcMotorControl.java
    Even shorter now :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/DcMotorControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/DcMotorControl.java
@@ -4,27 +4,17 @@ import com.qualcomm.robotcore.util.Range;
 
 public class DcMotorControl {
 
-    private static final double MIN_POWER_THRESHOLD = 0.125;
+    private static final double MIN_POWER_THRESHOLD = 0.35;
     private static final double MIN_STICK_THRESHOLD = 0.125;
     private static final double POWER_INCR = 0.25;
 
     public static double motorIncrControl(double analogStick, double currentPower) {
 
-        if (analogStick > MIN_STICK_THRESHOLD) {
-
-            double powerClip = Range.clip(currentPower, MIN_POWER_THRESHOLD, 1.0);
-            return (powerClip == currentPower + POWER_INCR) ? (currentPower + POWER_INCR) : powerClip;
-
-        } else if (analogStick < -MIN_STICK_THRESHOLD) {
-
-            double powerClip = Range.clip(currentPower, -1.0, -MIN_POWER_THRESHOLD);
-            return (powerClip == currentPower - POWER_INCR) ? (currentPower - POWER_INCR) : powerClip;
-
-        } else {
-
-            return 0.0;
-
-        }
+        return (analogStick > MIN_STICK_THRESHOLD)
+                ? Range.clip(currentPower + POWER_INCR, MIN_POWER_THRESHOLD, 1.0)
+                : ((analogStick < -MIN_STICK_THRESHOLD)
+                ? Range.clip(currentPower - POWER_INCR, -1.0, -MIN_POWER_THRESHOLD)
+                : 0.0);
 
     }
 
