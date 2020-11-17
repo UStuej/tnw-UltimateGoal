4812ddf1cadfd4bee4e4ef60f253ad88dc49d071
UStuej <bushytail4@gmail.com>
2020-11-16 19:53:34 -0600

    Update WallPhotos.java
    Transcribed the initial contour sorting and filtering code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WallPhotos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WallPhotos.java
@@ -2,14 +2,23 @@ package org.firstinspires.ftc.teamcode;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class WallPhotos {
 
-    /**Takes an image of a wall photo from the robot camera and returns either the estimated orientation of the robot given the difference in slopes between the sides of the contour enclosing the wall photo, or {@code null} if no such positive match was made
+    /** Takes an image of a wall photo from the robot camera and returns either the estimated orientation of the robot given the difference in slopes between the sides of the contour enclosing the wall photo, or {@code null} if no such positive match was made.
      */
     public static void getOrientationFromWallPhoto(final Mat img) {
 
@@ -32,6 +41,28 @@ public class WallPhotos {
             Imgproc.bilateralFilter(thresholded, bf, 11, 17.0, 17.0);
             Imgproc.Canny(bf, edges, 30.0, 200.0);
 
+        }
+
+        List<MatOfInt> contourHulls = new ArrayList<>();
+
+        {
+            List<MatOfPoint> contours = new ArrayList<>();
+            Imgproc.findContours(edges, contours, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+            Collections.sort(contours, new Comparator<Mat>() {
+                @Override
+                public int compare(Mat c0, Mat c1) {
+                    return (int) Math.round(Imgproc.contourArea(c1) - Imgproc.contourArea(c0));
+                }
+            });
+            contours = contours.subList(0, 10);
+
+            for (MatOfPoint contour : contours) {
+                MatOfInt contourHull = new MatOfInt();
+                Imgproc.convexHull(contour, contourHull);
+                if (Imgproc.contourArea(contourHull) > 9000.0)
+                    contourHulls.add(contourHull);
+            }
+            
         }
 
     }
