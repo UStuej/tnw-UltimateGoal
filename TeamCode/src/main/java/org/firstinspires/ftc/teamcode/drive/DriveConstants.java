a1e2703464e1ffbb56154d50a6ff4df0429a921a
BCubed603 <30398573+BCubed603@users.noreply.github.com>
2021-03-16 20:26:40 -0500

    Tuned Roadrunner Values
    Commit includes correctly tuned values for feedforward, heading PID, and translational PID as of 03/16/2021

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -40,8 +40,8 @@ public class DriveConstants {
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
     public static double WHEEL_RADIUS = 1.4763; // in
-    public static double GEAR_RATIO = .1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 15.0; // in
+    public static double GEAR_RATIO = 0.01485066403115189; // output (wheel) speed / input (motor) speed
+    public static double TRACK_WIDTH = 17.4; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -49,9 +49,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0;
-    public static double kStatic = 0;
+    public static double kV = 0.01485066403115189;
+    public static double kA = 0.004;
+    public static double kStatic = 0.005;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
