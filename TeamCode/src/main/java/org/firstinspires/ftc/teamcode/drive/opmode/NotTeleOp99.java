e6df8a6de8fd2b3f58f7e58125a99dd74550917f
BCubed603 <30398573+BCubed603@users.noreply.github.com>
2021-04-18 16:58:14 -0500

    Reverting ringElevator speed changes and adjusting lift height

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/NotTeleOp99.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/NotTeleOp99.java
@@ -22,8 +22,7 @@ public class NotTeleOp99 extends OpMode {
 
     private static int RING_ELEVATOR_UP_POSITION; // The position of the Ring Elevator when it is in the UP state
     private static int RING_ELEVATOR_DOWN_POSITION; // The position of the Ring Elevator when it is in the DOWN state
-    private static final double RING_ELEVATOR_POWER = 0.6; // The power for the motor to use when running to its target position
-    private static final double RING_ELEVATOR_MIN_POWER = 0.1;
+    private static final double RING_ELEVATOR_POWER = 1.0; // The power for the motor to use when running to its target position
 
     @Override
     public void init() {
@@ -31,7 +30,7 @@ public class NotTeleOp99 extends OpMode {
 
         // Set Ring Elevator motor...
         RING_ELEVATOR_DOWN_POSITION = ringElevator.getCurrentPosition();
-        RING_ELEVATOR_UP_POSITION = RING_ELEVATOR_DOWN_POSITION + 2017;
+        RING_ELEVATOR_UP_POSITION = RING_ELEVATOR_DOWN_POSITION + 2100;
         ringElevator.setTargetPosition(RING_ELEVATOR_DOWN_POSITION);
         ringElevator.setMode(DcMotor.RunMode.RUN_TO_POSITION); // run mode
     }
@@ -47,12 +46,9 @@ public class NotTeleOp99 extends OpMode {
         // Ring Elevator movement
         if (gamepad1APressed) {
             ringElevatorMovementIter++;
-            if (ringElevatorUp) ringElevator.setPower(Math.abs( (double) (ringElevator.getCurrentPosition() - RING_ELEVATOR_UP_POSITION) / (double) (RING_ELEVATOR_UP_POSITION - RING_ELEVATOR_DOWN_POSITION)) + RING_ELEVATOR_MIN_POWER);
-            else ringElevator.setPower(Math.abs( (double) (ringElevator.getCurrentPosition() - RING_ELEVATOR_DOWN_POSITION) / (double) (RING_ELEVATOR_UP_POSITION - RING_ELEVATOR_DOWN_POSITION)) + RING_ELEVATOR_MIN_POWER);
             ringElevator.setTargetPosition(ringElevatorUp ? RING_ELEVATOR_DOWN_POSITION : RING_ELEVATOR_UP_POSITION);
             ringElevatorUp = !ringElevatorUp;
         }
-
         telemetry.addData("Ring Elevator Up/Down Cycles: ", ringElevatorMovementIter / 2);
     }
 
