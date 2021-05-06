4cc1b0c1b2ed02764f4cbf86b544c434fb75ca49
UStuej <45504535+UStuej@users.noreply.github.com>
2021-05-06 17:16:56 -0500

    Create a test OpMode and edit a comment
    - Created a test Autonomous OpMode which determines the retention of static fields within a loaded class.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/StaticRetentionTest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "StaticRetentionTest", group = "test")
+
+public class StaticRetentionTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        telemetry.addData("Stored Time", TimeStorage.time);
+        telemetry.addData("Current Time", System.currentTimeMillis());
+        telemetry.update();
+
+        waitForStart();
+
+        TimeStorage.time = System.currentTimeMillis();
+        telemetry.addData("New Time", TimeStorage.time);
+        telemetry.update();
+
+        while (!isStopRequested()) {}
+
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/TimeStorage.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.test;
+
+public class TimeStorage {
+
+    public static long time = 0L;
+
+}
